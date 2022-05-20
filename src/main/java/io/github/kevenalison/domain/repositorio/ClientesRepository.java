19f2b9e8509e7f320745c7a483bf938edbdfffe7
KevenAlison <kevenalison13@gmail.com>
2022-05-20 02:37:27 -0300

    Mapeamento entidade produto

---
+++ src/main/java/io/github/kevenalison/domain/repositorio/ClientesRepository.java
@@ -0,0 +1,10 @@
+package io.github.kevenalison.domain.repositorio;
+
+import io.github.kevenalison.domain.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ClientesRepository extends JpaRepository<ClientesRepository, Integer> {
+
+}
