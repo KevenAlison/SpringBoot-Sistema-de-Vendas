5d4df769fcd3a13555d6fcab6602dad0ad7075d9
KevenAlison <kevenalison13@gmail.com>
2022-05-22 18:11:54 -0300

    Atualizar cliente

--- src/main/java/io/github/kevenalison/rest/controller/ClienteController.java
+++ src/main/java/io/github/kevenalison/rest/controller/ClienteController.java
@@ -48,4 +48,17 @@ public class ClienteController {
         return  ResponseEntity.notFound().build();
     }
 
+    @PutMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente){
+
+        return clientesRepository
+                .findById(id)
+                .map(clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientesRepository.save(cliente);
+                    return ResponseEntity.noContent().build();
+                 }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
